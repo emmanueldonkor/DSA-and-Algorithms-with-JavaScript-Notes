c11e41e551aa01ea63ca2de7f7e018a6c751d5f4
Emmanuel Donkor <edonkor0000@gmail.com>
2022-12-26 22:22:58 +0100

    Binary search recursion example

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -1,4 +1,4 @@
-function print(n){
+/*function print(n){
     console.log(n);
     print(n + 1);
 }
@@ -11,4 +11,25 @@ function fibo(n){
     }
     return fibo(n-1) + fibo(n-2);
 }
-console.log(fibo(6));
+console.log(fibo(6)); */
+
+const search = (arr, target, start, end) =>{
+    //The base case
+    if(start > end){
+        return -1;
+    }
+    let middle =parseInt(start+(end-start) /2)
+
+    if(arr[middle] === target){
+        return middle;
+    }
+    if(target < arr[middle]){
+        return search(arr, target, start, middle-1 )
+    }
+    return search(arr, target, middle+1, end )
+}
+let start = 0;
+let arr = [1,2,34,55,66,78]
+let end = arr.length;
+target = 55;
+console.log(search(arr, target, start, end))
